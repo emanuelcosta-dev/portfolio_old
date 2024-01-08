27dba314d9a1a9aa0905497e3bbc0c5feb5c2f56
Emanuel Costa <emanuelcosta28@outlook.com>
2024-01-08 22:58:09 +0000

    added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap"; // https://astro.build/config
+import vercelStatic from '@astrojs/vercel/static';
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://emanuelcosta.dev',
-  integrations: [tailwind(), image(), compress(), sitemap()]
+  integrations: [tailwind(), image(), compress(), sitemap()],
+  output: 'static',
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
 });
