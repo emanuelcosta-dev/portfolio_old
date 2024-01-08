33b33390d6a75149b59ae0408e3efd3b3006174f
Emanuel Costa <emanuelcosta28@outlook.com>
2024-01-08 22:39:31 +0000

    updated dependencies removed analytics errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap"; // https://astro.build/config
 
-import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-   site: 'https://emanuelcosta.dev',
-  integrations: [tailwind(), image(), compress(),  sitemap()]
+  site: 'https://emanuelcosta.dev',
+  integrations: [tailwind(), image(), compress(), sitemap()]
 });
